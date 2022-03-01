c2d3625654667ed91ebf512209bbb5f24f42a49c
418386910 <418386910@qq.com>
2022-03-01 19:43:36 +0800

    second commit

---
+++ src/main/java/com/yzhstudy/webapppractice/WebapppracticeApplication.java
@@ -0,0 +1,13 @@
+package com.yzhstudy.webapppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapppracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebapppracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzhstudy/webapppractice/WebapppracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzhstudy.webapppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapppracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
